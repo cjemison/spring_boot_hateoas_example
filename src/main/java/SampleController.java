eac6cdc3a0b02287d4f073a7432261da80fc8489
Cornelius Jemison <cornelius.jemison@gmail.com>
2015-05-04 12:34:30 -0500

    initial commit.

---
+++ src/main/java/SampleController.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+
+import org.springframework.hateoas.Resource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+
+@RestController
+public class SampleController {
+
+    @RequestMapping("/")
+    public HttpEntity<?> index() {
+
+        List<String> list = new ArrayList<String>();
+
+        int cnt = Math.abs(new Random().nextInt() % 50);
+        for (int i = 0; i < cnt; i++) {
+            list.add(UUID.randomUUID().toString());
+        }
+        Resource<Wrapper> resource = new Resource<>(new Wrapper(list));
+        resource.add(linkTo(SampleController.class).slash("/").withSelfRel());
+        return new ResponseEntity<>(resource, HttpStatus.OK);
+    }
+
+
+    private Resource<List<String>> toResource(final List<String> stringList) {
+        return new Resource<>(stringList, linkTo(SampleController.class).slash("/").withSelfRel());
+    }
+}
---
+++ src/main/java/Wrapper.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+
+public class Wrapper {
+
+    private List<String> content = null;
+
+    public Wrapper(List<String> content) {
+        this.content = content;
+    }
+
+    public List<String> getContent() {
+        return content;
+    }
+}
